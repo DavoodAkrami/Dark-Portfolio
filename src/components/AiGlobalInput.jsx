db2258dd72ea24e255e5aab6c13b77e1f38d1353
DavoodAkrami <davood.akrami.rk2025@gmail.com>
2025-12-11 23:09:06 +0330

    Fixing an error of AiGlobalInput

--- src/components/AiGlobalInput.jsx
+++ src/components/AiGlobalInput.jsx
@@ -45,27 +45,29 @@ const AiGlobalInput = () => {
 
     return (
         <div 
-            className='fixed bottom-5 w-full z-50'
+            className='fixed bottom-5 w-full z-50 pointer-events-none'
         >
             <form
                 onSubmit={handleSubmit}
-                className='relative flex items-center justify-center w-[18%] max-2xl:w-[30%] max-lg:w-[45%] max-md:w-[70%] max-sm:w-[80%] mx-auto transition-all ease-in-out duration-500 hover:scale-[1.1] focus-within:scale-[1.1] focus-within:w-[23%] max-2xl:focus-within:w-[35%] max-lg:focus-within:w-[54%] max-md:focus-within:w-[80%] '
+                className='relative flex items-center justify-center w-full transition-all ease-in-out duration-500 pointer-events-none'
             >
-                <input 
-                    type="text" 
-                    placeholder="Ask anything about Davood"
-                    value={inputValue}
-                    onChange={(e) => setInputValue(e.target.value)} 
-                    className='w-full cursor-pointer relative px-[0.7rem] pr-12 py-[0.7rem] !rounded-full bg-[var(--button-color)]/75 backdrop-blur-sm autofill:bg-[var(--primary-color)] ring-2 ring-[var(--accent-color)] outline-none focus:ring-2! focus:ring-[var(--accent-color)] max-md:h-[2.5rem]' 
-                />
-                <button 
-                    type="submit"
-                    className='absolute right-2 top-1/2 -translate-y-1/2 flex items-center justify-center w-9 h-9 rounded-full !shadow-none hover:!shadow-none cursor-pointer'
-                >
-                    <FaCircleArrowUp
-                        className='text-3xl text-[var(--accent-color)] hoverLight rounded-full'
+                <div className='relative w-[18%] max-2xl:w-[30%] max-lg:w-[45%] max-md:w-[70%] max-sm:w-[80%] mx-auto transition-all ease-in-out duration-500 hover:scale-[1.1] focus-within:scale-[1.1] focus-within:w-[23%] max-2xl:focus-within:w-[35%] max-lg:focus-within:w-[54%] max-md:focus-within:w-[80%] pointer-events-auto'>
+                    <input 
+                        type="text" 
+                        placeholder="Ask anything about Davood"
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)} 
+                        className='w-full cursor-pointer relative px-[0.7rem] pr-12 py-[0.7rem] !rounded-full bg-[var(--button-color)]/75 backdrop-blur-sm autofill:bg-[var(--primary-color)] ring-2 ring-[var(--accent-color)] outline-none focus:ring-2! focus:ring-[var(--accent-color)] max-md:h-[2.5rem]' 
                     />
-                </button>
+                    <button 
+                        type="submit"
+                        className='absolute right-2 top-1/2 -translate-y-1/2 flex items-center justify-center w-9 h-9 rounded-full !shadow-none hover:!shadow-none cursor-pointer'
+                    >
+                        <FaCircleArrowUp
+                            className='text-3xl text-[var(--accent-color)] hoverLight rounded-full'
+                        />
+                    </button>
+                </div>
             </form>
         </div>
     )
